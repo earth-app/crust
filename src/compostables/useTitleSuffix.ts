b41d18e8401ee4663e160eebf5422d118aa79ad3
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-06-25 21:20:59 -0500

    Create useTitleSuffix.ts

---
+++ src/compostables/useTitleSuffix.ts
@@ -0,0 +1,25 @@
+import { SITE_NAME } from './useConstants';
+
+export const useTitleSuffix = () => {
+	const suffix = useState<string>('titleSuffix', () => '');
+
+	const setTitleSuffix = (s: string) => {
+		suffix.value = s;
+		useHead({
+			titleTemplate: (title) => {
+				const clean = s.trim();
+				return title ? `${title} | ${clean}` : clean;
+			},
+			meta: [
+				{
+					name: 'og:title',
+					content: () => {
+						return suffix.value ? `${suffix.value} | ${SITE_NAME}` : SITE_NAME;
+					},
+				},
+			],
+		});
+	};
+
+	return { setTitleSuffix, suffix };
+};
