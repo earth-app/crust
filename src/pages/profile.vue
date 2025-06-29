087b9dc88e5359f56a328d8dedb39854752f2ee4
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-06-29 00:38:13 -0500

    feat: draft name + username profile page

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -3,11 +3,31 @@
 		v-if="user"
 		class="flex flex-row items-center justify-between"
 	>
-		<div class="flex flex-col items-center w-1/2 mt-6">
-			<h1 class="text-4xl font-bold mb-2">Profile</h1>
-			<p class="text-gray-600">Welcome to your profile page, {{ user.username }}!</p>
+		<div class="flex flex-col items-center w-full mt-6">
+			<h1 class="text-8xl font-bold mb-2">Profile</h1>
+			<div class="flex flex-row items-center space-x-1.5 mb-4">
+				<EditableValue
+					v-model="firstName"
+					class="text-3xl w-32"
+					size="xl"
+					placeholder="John"
+					:onFinish="updateUser"
+				/>
+				<EditableValue
+					v-model="lastName"
+					class="text-3xl w-32"
+					size="xl"
+					placeholder="Doe"
+					:onFinish="updateUser"
+				/>
+			</div>
+			<EditableValue
+				v-model="username"
+				class="text-xl"
+				size="xl"
+				:onFinish="updateUser"
+			/>
 		</div>
-		<div class="flex flex-col items-center w-1/2"></div>
 	</div>
 	<div
 		v-else
@@ -18,11 +38,51 @@
 </template>
 
 <script setup lang="ts">
-import { useAuth } from '~/compostables/useCurrentUser';
+import EditableValue from '~/components/EditableValue.vue';
+import { useAuth, updateAccount } from '~/compostables/useUser';
 import { useTitleSuffix } from '~/compostables/useTitleSuffix';
 
 const { user } = useAuth();
+const changed = ref(false);
 
 const { setTitleSuffix } = useTitleSuffix();
 setTitleSuffix('Profile');
+
+const accountProps = computed(() => {
+	if (!user.value?.account) return {};
+
+	return Object.keys(user.value.account).reduce((acc, key) => {
+		acc[key] = computed({
+			get: () => (user.value?.account as any)?.[key] ?? '',
+			set: (value) => {
+				if (user.value?.account) {
+					(user.value.account as any)[key] = value;
+					changed.value = true;
+				}
+			},
+		});
+		return acc;
+	}, {} as Record<string, any>);
+});
+
+const firstName = computed(() => accountProps.value.firstName);
+const lastName = computed(() => accountProps.value.lastName);
+const username = computed(() => accountProps.value.username);
+const email = computed(() => accountProps.value.email);
+const id = computed(() => accountProps.value.id);
+
+async function updateUser() {
+	if (user.value) {
+		if (!changed.value) return true;
+
+		const res = await updateAccount(user.value.account);
+		if (!res.success) {
+			return res.message || 'Failed to update profile.';
+		}
+
+		return true;
+	}
+
+	return 'User not found.';
+}
 </script>
