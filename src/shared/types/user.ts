9d64cecae77fdcc47bc9206c29f7360cd6a738cb
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-07-05 14:43:54 -0500

    chore: update `User` type

--- src/shared/types/user.ts
+++ src/shared/types/user.ts
@@ -1,7 +1,9 @@
+import { com } from '@earth-app/ocean';
+
 export type User = {
 	id: string;
-	fullName?: string;
 	username: string;
+	fullName?: string;
 	created_at: Date;
 	updated_at?: Date;
 	last_login?: Date;
@@ -11,14 +13,27 @@ export type User = {
 		firstName?: string;
 		lastName?: string;
 		username: string;
+		bio?: string;
 		email?: string;
-		address?: string | null;
+		address?: string;
 		country?: string;
 		phoneNumber?: number;
-		activities?: {
-			id: string;
-			name: string;
-			type: string;
-		}[];
+		visibility: {
+			account: typeof com.earthapp.Visibility.prototype.name;
+			name: typeof com.earthapp.account.Privacy.prototype.name;
+			bio: typeof com.earthapp.account.Privacy.prototype.name;
+			email: typeof com.earthapp.account.Privacy.prototype.name;
+			address: typeof com.earthapp.account.Privacy.prototype.name;
+			activities: typeof com.earthapp.account.Privacy.prototype.name;
+			events: typeof com.earthapp.account.Privacy.prototype.name;
+			friends: typeof com.earthapp.account.Privacy.prototype.name;
+			last_login: typeof com.earthapp.account.Privacy.prototype.name;
+			account_type: typeof com.earthapp.account.Privacy.prototype.name;
+		};
 	};
+	activities?: {
+		id: string;
+		name: string;
+		types: (typeof com.earthapp.activity.ActivityType.prototype.name)[];
+	}[];
 };
