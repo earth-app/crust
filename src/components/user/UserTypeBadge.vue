66a573518cafcf4d80367427878b9c08f046e096
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-07-28 15:32:01 -0500

    feat: `UserTypeBadge` component

---
+++ src/components/user/UserTypeBadge.vue
@@ -0,0 +1,36 @@
+<template>
+	<UBadge
+		v-if="props.user.account.account_type !== 'FREE'"
+		:label="badgeLabel"
+		:class="badgeStyling"
+		class="px-3 py-1 rounded-full text-white text-sm font-semibold"
+	/>
+</template>
+
+<script setup lang="ts">
+import type { User } from '~/shared/types/user';
+
+const props = defineProps<{
+	user: User;
+}>();
+
+const badgeLabel = computed(() => {
+	const type = props.user.account.account_type;
+	return type.at(0)?.toUpperCase() + type.slice(1).toLowerCase();
+});
+
+const badgeStyling = computed(() => {
+	switch (props.user.account.account_type) {
+		case 'ADMINISTRATOR':
+			return 'bg-red-500 font-bold';
+		case 'ORGANIZER':
+			return 'bg-green-500 font-semibold';
+		case 'WRITER':
+			return 'bg-yellow-500 font-medium';
+		case 'PRO':
+			return 'bg-blue-500';
+		default:
+			return 'bg-gray-500';
+	}
+});
+</script>
