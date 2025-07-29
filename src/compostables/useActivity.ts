2824d63be082ea9c9f70ed4cf98208ec194989b5
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-07-28 20:31:20 -0500

    Update useActivity.ts

--- src/compostables/useActivity.ts
+++ src/compostables/useActivity.ts
@@ -1,6 +1,6 @@
 import { makeAPIRequest, makeServerRequest, paginatedAPIRequest } from '~/shared/util';
 import { type Activity } from '~/shared/types/activity';
-import { getSessionToken } from './useLogin';
+import { useCurrentSessionToken } from './useLogin';
 
 // mantle - /v1/activities
 // cloud - /v1/activity
@@ -9,7 +9,7 @@ export async function getActivities(limit: number = 25, search: string = '') {
 	return await paginatedAPIRequest<Activity>(
 		`activities-${search}-${limit}`,
 		'/v1/activities',
-		getSessionToken(),
+		useCurrentSessionToken(),
 		{},
 		limit,
 		search
@@ -20,7 +20,7 @@ export async function getActivity(id: string) {
 	return await makeAPIRequest<Activity>(
 		`activity-${id}`,
 		`/v1/activities/${id}`,
-		getSessionToken()
+		useCurrentSessionToken()
 	);
 }
 
@@ -28,22 +28,27 @@ export async function draftActivity(id: string) {
 	return await makeServerRequest<Activity>(
 		`draft-activity-${id}`,
 		`/api/admin/draftActivity?id=${id}`,
-		getSessionToken()
+		useCurrentSessionToken()
 	);
 }
 
 export async function newActivity(activity: Activity) {
-	return await makeAPIRequest<Activity>(null, '/v1/activities/create', getSessionToken(), {
+	return await makeAPIRequest<Activity>(null, '/v1/activities/create', useCurrentSessionToken(), {
 		method: 'POST',
 		body: activity
 	});
 }
 
 export async function editActivity(activity: Activity) {
-	return await makeAPIRequest<Activity>(null, `/v1/activities/${activity.id}`, getSessionToken(), {
-		method: 'PATCH',
-		body: activity
-	});
+	return await makeAPIRequest<Activity>(
+		null,
+		`/v1/activities/${activity.id}`,
+		useCurrentSessionToken(),
+		{
+			method: 'PATCH',
+			body: activity
+		}
+	);
 }
 
 // Activity Icons
