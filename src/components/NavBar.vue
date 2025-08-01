61ae6c68b6c0a0d80b6f3ac792a09bb63c408dfa
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-07-31 23:56:26 -0500

    chore: move settings to own link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,20 +11,30 @@
 					class="h-12 w-12 inline-block mr-2 shadow-lg shadow-black/50 rounded-full hover:scale-105 transition-transform duration-300"
 				/>
 			</a>
+			<div class="flex items-center mr-12 space-x-4"></div>
 		</div>
 		<div class="ml-auto">
 			<div
 				v-if="user"
-				class="flex items-center justify-center space-x-1 cursor-pointer hover:opacity-80 transition-opacity duration-250"
-				@click="$router.push('/profile')"
+				class="flex items-center space-x-6"
 			>
-				<UAvatar
-					:src="avatarUrl"
-					class="w-12 h-12 rounded-full shadow-lg shadow-black/50"
+				<div
+					class="flex items-center justify-center space-x-1 cursor-pointer hover:opacity-80 transition-opacity duration-250"
+					@click="$router.push(`/profile/@${user.username}`)"
+				>
+					<UAvatar
+						:src="avatarUrl"
+						class="w-12 h-12 rounded-full shadow-lg shadow-black/50"
+					/>
+					<span class="font-title text-xl text-shadow-2xs text-shadow-black">{{
+						user.username
+					}}</span>
+				</div>
+				<UIcon
+					name="material-symbols:settings-rounded"
+					class="size-8 text-white cursor-pointer hover:scale-105 transition-transform duration-300"
+					@click="$router.push('/profile')"
 				/>
-				<span class="font-title text-xl text-shadow-2xs text-shadow-black">{{
-					user.username
-				}}</span>
 			</div>
 			<LoginPopup v-else />
 		</div>
