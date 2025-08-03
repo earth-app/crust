fd8bbbba853f5cff11d917fdf9864e9c46586a48
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-08-02 20:36:38 -0500

    feat: add last icon, reduce sizing reliance on width

--- src/components/EarthCircle.vue
+++ src/components/EarthCircle.vue
@@ -5,12 +5,12 @@
 		class="absolute h-full w-full rounded-full"
 	/>
 
-	<Icon
+	<UIcon
 		v-for="(icon, idx) in icons"
 		:key="idx"
 		:name="icon.name"
 		:title="`Satellite ${idx + 1}`"
-		size="calc(8px + 1.25vw)"
+		size="calc(24px + 0.8vw)"
 		class="absolute"
 		:style="iconStyle(idx, icon.offset, icon.radius)"
 	/>
@@ -20,14 +20,15 @@
 import { ref, onMounted, onUnmounted } from 'vue';
 
 const icons = [
-	{ name: 'carbon:satellite', speed: 6, radius: 60, offset: 0 },
-	{ name: 'solar:satellite-linear', speed: 8, radius: 80, offset: 45 },
-	{ name: 'circum:satellite-1', speed: 6, radius: 65, offset: 90 },
-	{ name: 'material-symbols-light:satellite-alt-outline', speed: 6.5, radius: 65, offset: 135 },
-	{ name: 'solar:rocket-bold-duotone', speed: 4, offset: 60, radius: 75 },
-	{ name: 'mdi:space-station', speed: 5, offset: 120, radius: 65 },
-	{ name: 'mdi:satellite-variant', speed: 7, offset: 240, radius: 65 },
-	{ name: 'solar:ufo-bold', speed: 4, offset: 300, radius: 80 }
+	{ name: 'carbon:satellite', speed: 6, radius: 65, offset: 0 },
+	{ name: 'solar:satellite-linear', speed: 8, radius: 85, offset: 45 },
+	{ name: 'circum:satellite-1', speed: 6, radius: 70, offset: 90 },
+	{ name: 'material-symbols-light:satellite-alt-outline', speed: 6.5, radius: 70, offset: 135 },
+	{ name: 'solar:rocket-bold-duotone', speed: 4, radius: 80, offset: 60 },
+	{ name: 'mdi:space-station', speed: 5, radius: 70, offset: 120 },
+	{ name: 'mdi:satellite-variant', speed: 7, radius: 70, offset: 240 },
+	{ name: 'solar:ufo-bold', speed: 4, radius: 85, offset: 300 },
+	{ name: 'mdi:space-invaders', speed: 7, radius: 90, offset: 180 }
 ];
 
 const angles = icons.map(() => ref(0));
