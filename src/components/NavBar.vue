fa8b98f7875ddf396f5c4ecabb1490370b5d7971
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-08-02 20:37:17 -0500

    chore: update dynamic sizing in NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,20 +3,27 @@
 		id="navbar"
 		class="bg-secondary-800 border-b-primary-500 border-b-8 text-white p-4 flex items-center"
 	>
-		<div class="flex items-center">
+		<div class="flex items-center w-2/3">
 			<a href="/">
 				<img
 					src="/favicon.png"
 					alt="Earth App Logo"
-					class="h-12 w-12 inline-block mr-2 shadow-lg shadow-black/50 rounded-full hover:scale-105 transition-transform duration-300"
+					class="min-h-8 min-w-8 size-8 xl:size-12 inline-block mr-2 shadow-lg shadow-black/50 rounded-full hover:scale-105 transition-transform duration-300"
 				/>
 			</a>
-			<div class="flex items-center mr-12 space-x-4"></div>
+			<Discover class="mx-2 sm:ml-4 md:ml-8 lg:ml-12" />
+			<div class="hidden sm:flex items-center mr-12 space-x-4">
+				<a
+					href="/activities"
+					class="text-md md:text-xl lg:text-2xl font-semibold hover:text-gray-300 mx-6"
+					>Activities</a
+				>
+			</div>
 		</div>
 		<div class="ml-auto">
 			<div
 				v-if="user"
-				class="flex items-center space-x-6"
+				class="flex items-center space-x-1 sm:space-x-6"
 			>
 				<div
 					class="flex items-center justify-center space-x-1 cursor-pointer hover:opacity-80 transition-opacity duration-250"
@@ -24,19 +31,20 @@
 				>
 					<UAvatar
 						:src="avatarUrl"
-						class="w-12 h-12 rounded-full shadow-lg shadow-black/50"
+						class="size-6 md:size-8 lg:size-12 rounded-full shadow-lg shadow-black/50"
 					/>
-					<span class="font-title text-xl text-shadow-2xs text-shadow-black">{{
-						user.username
-					}}</span>
+					<span
+						class="font-title text-md sm:text-lg md:text-xl text-shadow-2xs text-shadow-black"
+						>{{ user.username }}</span
+					>
 				</div>
 				<UIcon
 					name="material-symbols:settings-rounded"
-					class="size-8 text-white cursor-pointer hover:scale-105 transition-transform duration-300"
+					class="size-4 lg:size-8 text-white cursor-pointer hover:scale-105 transition-transform duration-300"
 					@click="$router.push('/profile')"
 				/>
 			</div>
-			<LoginPopup v-else />
+			<UserLoginPopup v-else />
 		</div>
 	</div>
 </template>
