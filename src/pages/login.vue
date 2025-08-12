f4458af09b53f4ffc563fec5aea1373c958c7402
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-08-12 03:29:17 -0500

    feat: login page

---
+++ src/pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="flex flex-col w-full h-full items-center justify-center px-60">
+		<h1 class="text-3xl font-semibold mb-4 mt-8">Login</h1>
+		<ClientOnly>
+			<UserLoginForm @login-success="$router.push('/')" />
+		</ClientOnly>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { useAuth } from '~/compostables/useUser';
+
+const { user } = useAuth();
+
+if (user.value) {
+	useRouter().push('/');
+}
+</script>
