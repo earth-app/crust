4e15fdab07ca69a55aacae1ed0d98ac6421cd9ae
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-08-13 20:52:27 -0500

    chore: add missing `aliases`, `fields` to `User` type

--- src/shared/types/user.ts
+++ src/shared/types/user.ts
@@ -39,6 +39,8 @@ export type User = {
 			name: string;
 			description?: string;
 			activity_types: (typeof com.earthapp.activity.ActivityType.prototype.name)[];
+			aliases: string[];
+			fields: Record<string, string>;
 		}[];
 	};
 };
