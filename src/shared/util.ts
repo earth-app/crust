c327ae30d7e6f0545d9042910873072fdd3c2f67
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-08-16 23:16:04 -0500

    feat(util): `withSuffix` method

--- src/shared/util.ts
+++ src/shared/util.ts
@@ -208,3 +208,10 @@ export function trimString(str: string, maxLength: number): string {
 
 	return str.slice(0, maxLength - 3) + '...';
 }
+
+export function withSuffix(num: number): string {
+	if (num >= 1e9) return `${(num / 1e9).toFixed(2)}B`;
+	if (num >= 1e6) return `${(num / 1e6).toFixed(2)}M`;
+	if (num >= 1e3) return `${(num / 1e3).toFixed(2)}K`;
+	return num.toString();
+}
