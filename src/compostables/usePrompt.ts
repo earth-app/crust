cdb79caae770d58002cf4438c841848fa3433ffd
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-09-14 20:34:09 -0500

    feat: add prompt & prompt response utility methods

--- src/compostables/usePrompt.ts
+++ src/compostables/usePrompt.ts
@@ -10,15 +10,31 @@ export async function getPrompt(id: string) {
 	return await util.makeClientAPIRequest<Prompt>(`/v2/prompts/${id}`);
 }
 
+export async function updatePrompt(id: string, prompt: string) {
+	return await util.makeClientAPIRequest<Prompt>(`/v2/prompts/${id}`, useCurrentSessionToken(), {
+		method: 'PATCH',
+		body: { prompt }
+	});
+}
+
+export async function removePrompt(id: string) {
+	return await util.makeClientAPIRequest<{ message: string }>(
+		`/v2/prompts/${id}`,
+		useCurrentSessionToken(),
+		{
+			method: 'DELETE'
+		}
+	);
+}
+
 export async function getPromptResponses(id: string, page: number = 1, limit: number = 25) {
-	return await util.makeClientAPIRequest<PromptResponse[]>(
+	return await util.makeClientAPIRequest<{ items: PromptResponse[] }>(
 		`/v2/prompts/${id}/responses?page=${page}&limit=${limit}`
 	);
 }
 
 export async function getPromptResponsesCount(id: string) {
-	return await util.makeAPIRequest<{ count: number; prompt: Prompt }>(
-		`responses-count-${id}`,
+	return await util.makeClientAPIRequest<{ count: number; prompt: Prompt }>(
 		`/v2/prompts/${id}/responses/count`
 	);
 }
@@ -30,6 +46,27 @@ export async function createPromptResponse(promptId: string, content: string) {
 		{
 			method: 'POST',
 			body: { content }
+		}
+	);
+}
+
+export async function updatePromptResponse(promptId: string, id: string, content: string) {
+	return await util.makeClientAPIRequest<PromptResponse>(
+		`/v2/prompts/${promptId}/responses/${id}`,
+		useCurrentSessionToken(),
+		{
+			method: 'PATCH',
+			body: { content }
+		}
+	);
+}
+
+export async function removePromptResponse(promptId: string, id: string) {
+	return await util.makeClientAPIRequest<{ message: string }>(
+		`/v2/prompts/${promptId}/responses/${id}`,
+		useCurrentSessionToken(),
+		{
+			method: 'DELETE'
 		}
 	);
 }
