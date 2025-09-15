e18fb69bd303623a11ae456423436bf703551e66
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-09-14 20:33:28 -0500

    feat: edit and delete buttons to `PromptCard` and `PromptResponse`

--- src/components/prompt/PromptCard.vue
+++ src/components/prompt/PromptCard.vue
@@ -2,7 +2,7 @@
 	<ClientOnly>
 		<div class="w-full flex items-center justify-center my-8">
 			<InfoCard
-				:title="prompt.prompt"
+				:title="promptText"
 				:description="author?.full_name"
 				:avatar="authorAvatar || 'https://cdn.earth-app.com/earth-app.png'"
 				avatar-size="xl"
@@ -12,15 +12,55 @@
 				:link="noLink ? undefined : `/prompts/${prompt.id}`"
 				:footer="`${footer} • ${responsesCount ? withSuffix(responsesCount) + ' Responses' : 'No Responses'}`"
 				:secondary-footer="secondaryFooter"
+				:buttons="
+					hasButtons
+						? [
+								{
+									text: 'Edit Prompt',
+									color: 'secondary',
+									onClick: () => {
+										editOpen = true;
+									}
+								},
+								{
+									text: 'Delete Prompt',
+									color: 'error',
+									onClick: () => {
+										deletePrompt();
+									}
+								}
+							]
+						: undefined
+				"
 			/>
 		</div>
+		<UModal
+			v-if="hasButtons"
+			title="Edit Prompt"
+			size="2xl"
+			:closeable="true"
+			v-model:open="editOpen"
+			:overlay="false"
+		>
+			<template #body>
+				<div class="flex flex-col space-y-4">
+					<UInput v-model="promptText" />
+					<UButton
+						@click="savePrompt"
+						:loading="editLoading"
+						:disabled="editLoading || promptText.trim().length === 0"
+						>Save</UButton
+					>
+				</div>
+			</template>
+		</UModal>
 	</ClientOnly>
 </template>
 
 <script setup lang="ts">
 import { DateTime } from 'luxon';
-import { getPromptResponsesCount } from '~/compostables/usePrompt';
-import { getUser, getUserAvatar } from '~/compostables/useUser';
+import { getPromptResponsesCount, removePrompt, updatePrompt } from '~/compostables/usePrompt';
+import { getUser, getUserAvatar, useAuth } from '~/compostables/useUser';
 import type { Prompt } from '~/shared/types/prompts';
 import { type User } from '~/shared/types/user';
 import { withSuffix } from '~/shared/util';
@@ -30,12 +70,22 @@ const props = defineProps<{
 	noLink?: boolean;
 }>();
 
+const promptText = ref(props.prompt.prompt);
 const author = ref<User | null>(null);
 const footer = ref<string | undefined>(undefined);
 const secondaryFooter = ref<string | undefined>(undefined);
 const authorAvatar = ref<string | null>(null);
 const authorAvatarChipColor = ref<any | null>(null);
 const responsesCount = ref<number | null>(null);
+
+watch(
+	() => props.prompt,
+	(newPrompt) => {
+		promptText.value = newPrompt.prompt;
+	}
+);
+
+const { user } = useAuth();
 
 const i18n = useI18n();
 const time = computed(() => {
@@ -45,6 +95,13 @@ const time = computed(() => {
 	});
 
 	return created.setLocale(i18n.locale.value).toLocaleString(DateTime.DATETIME_SHORT);
+});
+
+const hasButtons = computed(() => {
+	return (
+		user.value &&
+		(user.value.id === props.prompt.owner_id || user.value.account.account_type === 'ADMINISTRATOR')
+	);
 });
 
 const updatedTime = computed(() => {
@@ -97,4 +154,73 @@ onUnmounted(() => {
 		URL.revokeObjectURL(authorAvatar.value);
 	}
 });
+
+const editOpen = ref(false);
+const editLoading = ref(false);
+
+async function savePrompt() {
+	editLoading.value = true;
+	const toast = useToast();
+	const res = await updatePrompt(props.prompt.id, promptText.value);
+
+	if (res.success) {
+		editOpen.value = false;
+
+		toast.add({
+			title: 'Prompt Updated',
+			description: 'Your prompt has been successfully updated.',
+			icon: 'mdi:check',
+			color: 'success',
+			duration: 5000
+		});
+	} else {
+		toast.add({
+			title: 'Error',
+			description: res.message || 'An unknown error occurred while updating your prompt.',
+			icon: 'mdi:alert-circle',
+			color: 'error',
+			duration: 7000
+		});
+	}
+
+	editLoading.value = false;
+}
+
+async function deletePrompt() {
+	const yes = confirm('Are you sure you want to delete this prompt? This action cannot be undone.');
+
+	const toast = useToast();
+	const router = useRouter();
+	if (yes) {
+		const res = await removePrompt(props.prompt.id);
+		if (res.success) {
+			toast.add({
+				title: 'Prompt Deleted',
+				description: 'Your prompt has been successfully deleted.',
+				icon: 'mdi:check',
+				color: 'success',
+				duration: 5000
+			});
+
+			refreshNuxtData(`prompt-${props.prompt.id}`);
+			router.push('/prompts');
+		} else {
+			toast.add({
+				title: 'Error',
+				description: res.message || 'An unknown error occurred while deleting your prompt.',
+				icon: 'mdi:alert-circle',
+				color: 'error',
+				duration: 7000
+			});
+		}
+	} else {
+		toast.add({
+			title: 'Cancelled',
+			description: 'Prompt deletion has been cancelled.',
+			icon: 'mdi:cancel',
+			color: 'info',
+			duration: 5000
+		});
+	}
+}
 </script>
--- src/components/prompt/PromptResponse.vue
+++ src/components/prompt/PromptResponse.vue
@@ -1,42 +1,86 @@
 <template>
 	<ClientOnly>
-		<InfoCard
-			:external="false"
-			variant="subtle"
-			:title="identifier"
-			:content="response.response"
-			:footer="time"
-			:footer-tooltip="
-				response.updated_at && response.created_at.toString() !== response.updated_at?.toString()
-					? `${response.created_at} (edited ${response.updated_at})`
-					: response.created_at
-			"
-			:secondary-footer="response.id"
-			:avatar="userAvatar || 'https://cdn.earth-app.com/earth-app.png'"
-			avatar-size="md"
-			:avatar-chip="user?.account.account_type ? true : false"
-			:avatar-chip-color="
-				user?.account.account_type === 'ORGANIZER'
-					? 'warning'
-					: user?.account.account_type === 'ADMINISTRATOR'
-						? 'error'
+		<div class="w-full flex items-center justify-center my-8">
+			<InfoCard
+				:external="false"
+				variant="subtle"
+				:title="identifier"
+				:content="responseText"
+				:footer="time"
+				:footer-tooltip="tooltip"
+				:avatar="userAvatar || 'https://cdn.earth-app.com/earth-app.png'"
+				avatar-size="md"
+				:avatar-chip="user?.account.account_type ? true : false"
+				:avatar-chip-color="
+					user?.account.account_type === 'ORGANIZER'
+						? 'warning'
+						: user?.account.account_type === 'ADMINISTRATOR'
+							? 'error'
+							: undefined
+				"
+				:buttons="
+					hasButtons
+						? [
+								{
+									text: 'Edit Response',
+									color: 'secondary',
+									onClick: () => {
+										editOpen = true;
+									}
+								},
+								{
+									text: 'Delete Response',
+									color: 'error',
+									onClick: () => {
+										deleteResponse();
+									}
+								}
+							]
 						: undefined
-			"
-		/>
+				"
+			/>
+		</div>
+		<UModal
+			v-if="hasButtons"
+			title="Edit Prompt Response"
+			size="2xl"
+			:closeable="true"
+			v-model:open="editOpen"
+			:overlay="false"
+		>
+			<template #body>
+				<div class="flex flex-col space-y-4">
+					<h2 class="font-medium">New Response:</h2>
+					<UInput v-model="responseText" />
+					<UButton
+						@click="saveResponse"
+						:loading="editLoading"
+						:disabled="editLoading || responseText.trim().length === 0"
+						>Save</UButton
+					>
+				</div>
+			</template>
+		</UModal>
 	</ClientOnly>
 </template>
 
 <script setup lang="ts">
 import { DateTime } from 'luxon';
-import { getUser, getUserAvatar } from '~/compostables/useUser';
-import type { PromptResponse } from '~/shared/types/prompts';
+import { removePromptResponse, updatePromptResponse } from '~/compostables/usePrompt';
+import { getUserAvatar } from '~/compostables/useUser';
+import { type PromptResponse } from '~/shared/types/prompts';
 import type { User } from '~/shared/types/user';
 
 const props = defineProps<{
 	response: PromptResponse;
 }>();
 
+const emit = defineEmits<{
+	(event: 'deleted'): void;
+}>();
+
 const i18n = useI18n();
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 const user = ref<User | null>(null);
 const identifier = computed(
 	() => user.value?.full_name || `@${user.value?.username || 'anonymous'}`
@@ -62,17 +106,25 @@ const time = computed(() => {
 	return str || 'Whenever';
 });
 
+const tooltip = computed(() => {
+	const created = DateTime.fromISO(props.response.created_at, {
+		zone: timezone
+	}).toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+
+	if (props.response.updated_at && props.response.created_at !== props.response.updated_at) {
+		const updated = DateTime.fromISO(props.response.updated_at, {
+			zone: timezone
+		}).toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+		return `${created} (edited ${updated})`;
+	}
+
+	return created;
+});
+
 onMounted(async () => {
-	if (props.response.owner_id) {
-		user.value = await getUser(props.response.owner_id).then((res) => {
-			if (res.success && res.data) {
-				return res.data;
-			}
-
-			return null;
-		});
-
-		userAvatar.value = await getUserAvatar(props.response.owner_id).then((res) => {
+	if (props.response.owner) {
+		user.value = props.response.owner;
+		userAvatar.value = await getUserAvatar(props.response.owner.id).then((res) => {
 			if (res.success && res.data) {
 				const blob = res.data;
 				return URL.createObjectURL(blob);
@@ -87,4 +139,94 @@ onUnmounted(() => {
 		URL.revokeObjectURL(userAvatar.value);
 	}
 });
+
+const hasButtons = computed(() => {
+	return (
+		user.value &&
+		(user.value.id === props.response.owner.id ||
+			user.value.account.account_type === 'ADMINISTRATOR')
+	);
+});
+
+const responseText = ref(props.response.response);
+watch(
+	() => props.response,
+	(newResponse) => {
+		responseText.value = newResponse.response;
+	}
+);
+
+const editOpen = ref(false);
+const editLoading = ref(false);
+
+async function saveResponse() {
+	editLoading.value = true;
+	const toast = useToast();
+	const res = await updatePromptResponse(
+		props.response.prompt_id,
+		props.response.id,
+		responseText.value
+	);
+
+	if (res.success) {
+		editOpen.value = false;
+
+		toast.add({
+			title: 'Prompt Response Updated',
+			description: 'Your prompt response has been successfully updated.',
+			icon: 'mdi:check',
+			color: 'success',
+			duration: 5000
+		});
+	} else {
+		toast.add({
+			title: 'Error',
+			description: res.message || 'An unknown error occurred while updating your prompt response.',
+			icon: 'mdi:alert-circle',
+			color: 'error',
+			duration: 7000
+		});
+	}
+
+	editLoading.value = false;
+}
+
+async function deleteResponse() {
+	const yes = confirm(
+		'Are you sure you want to delete this response? This action cannot be undone.'
+	);
+
+	const toast = useToast();
+	if (yes) {
+		const res = await removePromptResponse(props.response.prompt_id, props.response.id);
+		if (res.success) {
+			toast.add({
+				title: 'Prompt Response Deleted',
+				description: 'Your prompt response has been successfully deleted.',
+				icon: 'mdi:check',
+				color: 'success',
+				duration: 5000
+			});
+
+			emit('deleted');
+		} else {
+			toast.add({
+				title: 'Error',
+				description:
+					res.message || 'An unknown error occurred while deleting your prompt response.',
+				icon: 'mdi:alert-circle',
+				color: 'error',
+				duration: 7000
+			});
+		}
+	} else {
+		toast.add({
+			title: 'Cancelled',
+			description: 'Prompt deletion has been cancelled.',
+			icon: 'mdi:cancel',
+			color: 'info',
+			duration: 5000
+		});
+	}
+}
 </script>
