f4e9299fd4db93b8a241da081166607b1752b390
Gregory Mitchell <me@gmitch215.xyz>
2025-09-30 18:03:33 +0000

    chore: auto redirect from login if signed in

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -2,7 +2,7 @@
 	<div class="flex flex-col w-full h-full items-center justify-center px-60">
 		<h1 class="text-3xl font-semibold mb-4 mt-8">Login</h1>
 		<ClientOnly>
-			<UserLoginForm @login-success="$router.push('/')" />
+			<UserLoginForm @login-success="handleLoginSuccess" />
 		</ClientOnly>
 	</div>
 </template>
@@ -12,10 +12,26 @@ import { useAuth } from '~/compostables/useUser';
 
 const { user } = useAuth();
 
+onMounted(() => {
+	if (user.value) {
+		// Redirect to home page or dashboard if already logged in
+		const toast = useToast();
+		useRouter().push('/');
+
+		toast.add({
+			title: 'Already Logged In',
+			description: 'You are already logged in.',
+			icon: 'mdi:login-variant',
+			color: 'info',
+			duration: 3000
+		});
+	}
+});
+
 function handleLoginSuccess() {
 	if (user.value) {
 		// Redirect to home page or dashboard after successful login
-		window.location.href = '/';
+		useRouter().push('/');
 		refreshNuxtData(['user-current', 'avatar-current']); // Refresh user data
 	}
 }
