ac06594a6e91cec01c51672eb34d2a258dd82099
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-10-05 21:25:56 -0500

    feat: update about page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -5,10 +5,18 @@
 			<EarthCircle />
 		</div>
 		<p class="text-md mt-2 text-center max-w-3xl">
-			The Earth App is a new take on the app landscape, encouraging users to learn about new things
-			and find novelty in their lives. The app appeals to intellectual curiosity, providing fresh
-			new questions, insights, and topics every day. Learn and explore more about the world with The
-			Earth App.
+			In today's world, we are often confined within our own lives, isolated from the vastness of
+			knowledge and experience that surrounds us. The Earth App is a new take on the app landscape,
+			encouraging users to learn about new things and find novelty in their lives. The app appeals
+			to intellectual curiosity, providing fresh new questions, insights, and topics every day.
+			Learn and explore more about the world with The Earth App. and find novelty in their lives.
+			The app appeals to intellectual curiosity, providing fresh new questions, insights, and topics
+			every day. Learn and explore more about the world with The Earth App. Our goal is to help
+			users break free from this loneliness by providing a platform that encourages exploration and
+			discovery. By using The Earth App, users can expand their horizons and connect with the world
+			around them in a meaningful way. You can support this app by following us on social media and
+			sharing it with your friends. Together, we can create a community of curious minds and explore
+			the wonders of the world around us.
 		</p>
 		<h1 class="text-2xl font-bold mt-4">Creator</h1>
 		<div class="flex justify-center w-2/3">
@@ -27,11 +35,12 @@
 				</div>
 				<div class="flex flex-col items-center max-w-3xl ml-8 mt-12 w-1/2">
 					<p class="text-md mt-2">
-						Gregory Mitchell is a software developer and student based in the United States. He has
-						a passion for building applications that make a difference in people's lives, and he
-						believes that technology can be a powerful tool for learning, exploration, and improving
-						mental health. Gregory is dedicated to creating an app that encourages users to step
-						outside their comfort zones and discover new things about the world around them.
+						Gregory Mitchell is a 17-year-old software developer and student based in the United
+						States. Born in Illinois, he has a passion for building applications that make a
+						difference in people's lives, and he believes that technology can be a powerful tool for
+						learning, exploration, and improving mental health. Gregory is dedicated to creating an
+						app that encourages users to step outside their comfort zones and discover new things
+						about the world around them. You can look at his other projects on his website:
 					</p>
 					<NuxtLink
 						to="https://gmitch215.dev"
