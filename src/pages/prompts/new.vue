953c263e48fd9c5c4bd9a7123e9ea6a440e5bf88
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-10-29 17:01:16 -0500

    chore: prevent private accounts from creating prompts

--- src/pages/prompts/new.vue
+++ src/pages/prompts/new.vue
@@ -8,6 +8,22 @@
 import type { Prompt } from '~/shared/types/prompts';
 
 const router = useRouter();
+const toast = useToast();
+const { user } = useAuth();
+
+onMounted(() => {
+	if (user.value && user.value.account.visibility === 'PRIVATE') {
+		router.push('/profile');
+
+		toast.add({
+			title: 'Profile Private',
+			description: 'You need to set your profile to Public or Unlisted to create prompts.',
+			icon: 'mdi:account-alert',
+			color: 'warning',
+			duration: 5000
+		});
+	}
+});
 
 function moveToPrompt(prompt: Prompt) {
 	router.push(`/prompts/${prompt.id}`);
