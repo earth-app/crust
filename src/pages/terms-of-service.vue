0514713202ce82b8247b5979266ba59063311174
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-03-04 12:21:50 -0600

    chore: update tos

--- src/pages/terms-of-service.vue
+++ src/pages/terms-of-service.vue
@@ -5,43 +5,270 @@
 			<EarthCircle />
 		</div>
 		<div class="text-left">
+			<p class="text-md mt-2 max-w-3xl">Last Updated: March 4, 2026</p>
 			<p class="text-md mt-2 max-w-3xl">
 				The Earth App ("we", "our", or "us") provides a platform for users to explore and learn
 				about the world around them. By accessing or using our services, you agree to comply with
 				and be bound by the following terms and conditions ("Terms of Service"). Please read these
 				terms carefully before using our services.
 			</p>
-			<h1 class="text-2xl font-bold mt-4">1. Acceptance of Terms</h1>
+			<h1 class="text-2xl font-bold mt-4">1. General Provisions</h1>
+			<p class="text-md mt-2 max-w-3xl">
+				The Earth App is subject to United States export, copyright, control, and criminal laws and
+				regulations. You agree to comply with all applicable export control laws and regulations,
+				including but not limited to the U.S. Export Administration Regulations and the U.S.
+				International Traffic in Arms Regulations, as well as any applicable export control laws and
+				regulations of other countries. You shall not export, re-export, or transfer any content,
+				software, or technology obtained from our services in violation of any applicable export
+				control laws and regulations. You are responsible for obtaining any necessary licenses or
+				permissions required to export, re-export, or transfer content, software, or technology
+				obtained from our services. We reserve the right to deny access to our services or to any
+				content, software, or technology obtained from our services to any person or entity that we
+				believe may be in violation of applicable export control laws and regulations. The Earth App
+				is subject to Illinois (IL) jurisdiction, and any disputes arising out of or related to
+				these Terms of Service or our services shall be resolved in accordance with IL and US law.
+				By using our services, you consent to the exclusive jurisdiction and venue of courts located
+				in the Cook County, Illinois for the resolution of any disputes arising out of or related to
+				these Terms of Service or our services.
+			</p>
+			<p class="text-md mt-2 max-w-3xl">
+				In the event of a class action lawsuit against us, you agree that you will not participate
+				as a plaintiff or class member in any class action lawsuit against us. You agree that the
+				only claims you may bring against us are individual claims, and you waive your right to
+				bring any class action lawsuit against us. If a court determines that the waiver of class
+				action rights is unenforceable, then you agree to resolve any disputes with us through
+				arbitration on an individual basis, rather than in a class action, and you waive your right
+				to participate in a class action lawsuit or class-wide arbitration against us. If you have
+				any questions about this provision, please contact us at
+				<NuxtLink
+					to="mailto:support@earth-app.com"
+					class="text-blue-500 hover:underline"
+					>support@earth-app.com</NuxtLink
+				>.
+			</p>
+			<p class="text-md mt-2 max-w-3xl">
+				This Terms of Service is provided in English, AS IS, and we make no representations or
+				warranties of any kind, express or implied, about the completeness, accuracy, reliability,
+				suitability, or availability of the services or the information, products, services, or
+				related graphics contained in the services for any purpose. Any reliance you place on such
+				information is therefore strictly at your own risk. In no event will we be liable for any
+				loss or damage including without limitation, indirect or consequential loss or damage, or
+				any loss or damage whatsoever arising from loss of data or profits arising out of, or in
+				connection with, the use of our services.
+			</p>
+			<h1 class="text-2xl font-bold mt-4">2. Acceptance of Terms</h1>
 			<p class="text-md mt-2 max-w-3xl">
 				By using our services, you agree to be bound by these Terms of Service, as well as our
 				Privacy Policy. If you do not agree to these terms, please do not use our services.
 			</p>
-			<h1 class="text-2xl font-bold mt-4">2. User Conduct</h1>
+			<p class="text-md mt-2 max-w-3xl">
+				Advertising your own products, services, or content on our platform is not allowed without
+				our prior written consent. Those with the consent of The Earth App may be allowed to
+				advertise their own content, but must still adhere to all other guidelines and policies.
+			</p>
+			<p class="text-md mt-2 max-w-3xl">
+				To comply with the Children's Online Privacy Protection Act (COPPA) and relevant
+				international laws, we do not knowingly collect personal information from children under the
+				age of 13, or the age of 16 in certain jurisdictions. Our services are not directed towards
+				children, and we do not encourage children to use our services without parental consent. If
+				we become aware that we have collected personal information from a child under the
+				applicable age, we will take steps to delete that information as soon as possible. If you
+				are a parent or guardian and believe that your child has provided us with personal
+				information without your consent, please contact us at
+				<NuxtLink
+					to="mailto:support@earth-app.com"
+					class="text-blue-500 hover:underline"
+				>
+					support@earth-app.com
+				</NuxtLink>
+				so that we can take appropriate action.
+			</p>
+			<p class="text-md mt-2 max-w-3xl">
+				Administrators and moderators of our services are responsible for enforcing these Terms of
+				Service and maintaining a safe and respectful environment for all users. They have the
+				authority to remove content, issue warnings, and take other actions as necessary to enforce
+				our policies. They also have permission to view all private information provided by users,
+				including, but not limited to: email addresses, physical address, phone numbers, and any
+				other personally identifiable information. You may opt to withhold this information on your
+				profile. However, if you choose to provide it, you acknowledge and consent to administrators
+				and moderators having access to this information for the purpose of enforcing our policies
+				and ensuring the safety of our community. They have permission to access this information at
+				any time, and may use it to contact you if necessary regarding your account or any content
+				you have submitted. We take the privacy and security of our users' information seriously and
+				have implemented measures to protect it. However, we cannot guarantee the absolute security
+				of any information you provide to us, and you acknowledge and accept the risks associated
+				with sharing personal information online.
+			</p>
+			<h1 class="text-2xl font-bold mt-4">3. User Conduct</h1>
 			<p class="text-md mt-2 max-w-3xl">
 				You agree to use our services in a manner that is lawful, ethical, and respectful of others.
 				You shall not engage in any activity that may harm, disrupt, or interfere with the proper
 				functioning of our services or the experience of other users.
 			</p>
-			<h1 class="text-2xl font-bold mt-4">3. Intellectual Property</h1>
+			<p class="text-md mt-2 max-w-3xl">
+				Any photographs, writing, videos, or other content that you submit to our services ("User
+				Content") must comply with our content guidelines and must not violate any applicable laws
+				or regulations. You are solely responsible for the User Content you submit and must ensure
+				that it does not infringe upon the rights of any third party, including intellectual
+				property rights, privacy rights, and publicity rights. We reserve the right to remove any
+				User Content that we deem inappropriate or in violation of our policies.
+			</p>
+			<p class="text-md mt-2 max-w-3xl">
+				All content involving other persons must have the consent of those individuals before being
+				submitted to our services. By submitting such content, you represent and warrant that you
+				have obtained all necessary consents and permissions from the individuals depicted in the
+				content. We reserve the right to remove any content that we believe may violate this
+				requirement or that we deem inappropriate for our platform. Users may request removal of
+				content involving themselves by contacting us at
+				<NuxtLink
+					to="mailto:support@earth-app.com"
+					class="text-blue-500 hover:underline"
+				>
+					support@earth-app.com </NuxtLink
+				>. If you fail to obtain the necessary consents and permissions for content involving other
+				persons, you may be held liable for any resulting claims or damages. We encourage all users
+				to respect the privacy and rights of others when submitting content to our services. You
+				agree to indemnify and hold harmless The Earth App and its affiliates, officers, agents, and
+				employees from any claim, demand, loss, liability, or expense arising out of or related to
+				your violation of this requirement or your failure to obtain the necessary consents and
+				permissions for content involving other persons, or any other violation of these Terms of
+				Service.
+			</p>
+			<p class="text-md mt-2 max-w-3xl">
+				In accordance with the Digital Millennium Copyright Act (DMCA) and other applicable laws, we
+				have a process for responding to claims of copyright infringement. If you believe that your
+				copyrighted work has been infringed upon through our services, you may submit a DMCA
+				takedown notice to us at
+				<NuxtLink
+					to="mailto:support@earth-app.com"
+					class="text-blue-500 hover:underline"
+				>
+					support@earth-app.com </NuxtLink
+				>. Your notice must include the following information: (1) a physical or electronic
+				signature of the copyright owner or a person authorized to act on their behalf; (2)
+				identification of the copyrighted work claimed to have been infringed; (3) identification of
+				the material that is claimed to be infringing and that is to be removed or access to which
+				is to be disabled, along with information reasonably sufficient to permit us to locate the
+				material; (4) your contact information, including your address, telephone number, and email
+				address; (5) a statement that you have a good faith belief that the use of the material in
+				the manner complained of is not authorized by the copyright owner, its agent, or the law;
+				and (6) a statement that the information in the notification is accurate and, under penalty
+				of perjury, that you are authorized to act on behalf of the copyright owner. We will review
+				all DMCA takedown notices and take appropriate action as required by law.
+			</p>
+			<p class="text-md mt-2 max-w-3xl">
+				We will keep a temporary record of all content submitted to our services, including User
+				Content and AI-generated, even if it is publicly removed, for a period of 30 days. If you
+				would like for it to be removed permanently in compliance with the General Data Protection
+				Regulation (GDPR) or other applicable laws, or wish to appeal the removal of content, please
+				contact us at
+				<NuxtLink
+					to="mailto:support@earth-app.com"
+					class="text-blue-500 hover:underline"
+				>
+					support@earth-app.com</NuxtLink
+				>
+				with a valid explanation. We will review all permanent demands and appeals and may reinstate
+				content if we determine that it was removed in error or if we receive additional information
+				that changes our assessment. However, we reserve the right to uphold content removals if we
+				believe they were justified based on our policies and guidelines. Failure to appeal content
+				removals within 30 days may result in the permanent deletion of the content from our
+				records. Content that is requested to be deleted permanently will be removed from our
+				services and all associated records, and we will not retain any copies of the content after
+				the deletion process is complete, except as required by law. We encourage all users to
+				adhere to these Terms of Service and to respect the rights and privacy of others when
+				submitting content to our services.
+			</p>
+			<h1 class="text-2xl font-bold mt-4">4. Intellectual Property</h1>
 			<p class="text-md mt-2 max-w-3xl">
 				All content and materials provided through our services, including but not limited to text,
 				graphics, logos, images, and software, are the property of The Earth App or its licensors
 				and are protected by intellectual property laws. You may not use, reproduce, or distribute
 				any content from our services without our prior written consent.
 			</p>
-			<h1 class="text-2xl font-bold mt-4">4. Limitation of Liability</h1>
+			<p class="text-md mt-2 max-w-3xl">
+				By submitting User Content to our services, you grant us a worldwide, non-exclusive,
+				royalty-free, sublicensable, and transferable license to use, reproduce, distribute, prepare
+				derivative works of, display, and perform the User Content in connection with our services
+				and our business operations. You also grant each user of our services a non-exclusive
+				license to access your User Content through our services and to use it in accordance with
+				the functionality of our services. This includes, but is not limited to, the right for users
+				to view, share, and interact with your User Content as permitted by the features of our
+				platform. You represent and warrant that you have all necessary rights and permissions to
+				grant the above licenses for any User Content you submit, and that your User Content does
+				not infringe upon the intellectual property rights of any third party. We reserve the right
+				to remove any User Content that we believe may violate this requirement or that we deem
+				inappropriate for our platform.
+			</p>
+			<p class="text-md mt-2 max-w-3xl">
+				Users have the ability to set their accounts to "Unlisted" or "Private," which limits the
+				visibility of their content to only those with whom they choose to share it. If you set your
+				account to "Unlisted," your content will not be discoverable through search or browsing
+				features, but can still be accessed by anyone with the direct link. If you set your account
+				to "Private," your content will only be visible to users you have approved as followers, and
+				will not be accessible to anyone else. In addition, provided content like names, email
+				addresses, and phone numbers can also be set to "Private," "Public," "Mutual," or "Circle,"
+				which controls who can see that information on your profile. We respect users' privacy
+				settings and will not share or disclose content that is set to "Unlisted" or "Private"
+				without your consent, except as required by law or to protect our rights. Use of user
+				content for advertising purposes as described above will be omitted for content from
+				accounts set to "Unlisted" or "Private." We do not guarantee the security of any content you
+				choose to share on our platform, and you acknowledge and accept the risks associated with
+				sharing content online, even if it is set to "Unlisted" or "Private." We encourage all users
+				to carefully consider the content they share and to use our privacy settings to control who
+				can see their content.
+			</p>
+			<p class="text-md mt-2 max-w-3xl">
+				Accounts found in violation of any section of these Terms of Service may be subject to
+				penalties, including but not limited to content removal, account suspension, or permanent
+				banning. We reserve the right to take any action we deem necessary to enforce our policies
+				and maintain a safe and respectful environment for all users. If you believe that your
+				account has been unfairly penalized, you may contact us at
+				<NuxtLink
+					to="mailto:support@earth-app.com"
+					class="text-blue-500 hover:underline"
+				>
+					support@earth-app.com
+				</NuxtLink>
+				with an explanation of the situation. We will review all appeals and may reinstate accounts
+				if we determine that they were penalized in error or if we receive additional information
+				that changes our assessment. However, we reserve the right to uphold penalties if we believe
+				they were justified based on our policies and guidelines. We encourage all users to adhere
+				to these Terms of Service and to respect the rights and privacy of others when using our
+				services.
+			</p>
+			<h1 class="text-2xl font-bold mt-4">5. Limitation of Liability</h1>
 			<p class="text-md mt-2 max-w-3xl">
 				To the fullest extent permitted by law, The Earth App shall not be liable for any direct,
 				indirect, incidental, special, consequential, or punitive damages arising out of or related
 				to your use of our services.
 			</p>
-			<h1 class="text-2xl font-bold mt-4">5. Changes to Terms</h1>
+			<p class="text-md mt-2 max-w-3xl">
+				The application allows users to submit their own content, and we do not endorse or take
+				responsibility for any user-generated content. Users are solely responsible for the content
+				they submit and must ensure that it does not violate any applicable laws or regulations. We
+				reserve the right to remove any user-generated content that we deem inappropriate or in
+				violation of our policies.
+			</p>
+			<p class="text-md mt-2 max-w-3xl">
+				In addition to user-generated content, our services may include AI-generated content. The
+				parameters for generating this content are determined by us, and we do not guarantee the
+				accuracy, reliability, or appropriateness of any AI-generated content. Users should exercise
+				caution and discretion when interacting with AI-generated content and should not rely on it
+				for critical decisions or actions. In addition, we reserve the right to remove or modify any
+				AI-generated content that we deem inappropriate or in violation of our policies.
+			</p>
+			<h1 class="text-2xl font-bold mt-4">6. Changes to Terms</h1>
 			<p class="text-md mt-2 max-w-3xl">
 				We reserve the right to modify or update these Terms of Service at any time. Any changes
 				will be effective immediately upon posting on our website. Your continued use of our
-				services after any such changes constitutes your acceptance of the new terms.
+				services after any such changes constitutes your acceptance of the new terms. We will
+				endeavor to notify users of significant changes to these Terms of Service through our
+				website or other communication channels. It is your responsibility to review these Terms of
+				Service periodically to stay informed about any updates or changes. If you do not agree to
+				the updated terms, please discontinue use of our services.
 			</p>
-			<h1 class="text-2xl font-bold mt-4">6. Contact Us</h1>
+			<h1 class="text-2xl font-bold mt-4">7. Contact Us</h1>
 			<p class="text-md mt-2 max-w-3xl">
 				If you have any questions or concerns about these Terms of Service, please contact us at
 				<NuxtLink
