c1687cb28ed62ae4b504acbbc1edddb57737315c
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-04-15 10:09:28 -0500

    feat: add prompt page tour

--- src/components/prompt/Page.vue
+++ src/components/prompt/Page.vue
@@ -8,6 +8,7 @@
 			/>
 			<div class="flex w-full justify-center items-center my-2">
 				<UTextarea
+					id="response-input"
 					v-model="newResponse"
 					:avatar="
 						user
@@ -26,20 +27,29 @@
 					:disabled="posting || !user"
 					@keyup.enter="postResponse"
 				/>
-				<UButton
-					class="ml-4"
-					color="secondary"
-					:loading="posting"
-					:disabled="posting || !user || newResponse.trim().length === 0"
-					@click="postResponse"
-					>Post</UButton
-				>
+				<div class="flex ml-4 gap-1">
+					<UButton
+						id="post-button"
+						color="secondary"
+						:loading="posting"
+						:disabled="posting || !user || newResponse.trim().length === 0"
+						@click="postResponse"
+						>Post</UButton
+					>
+					<UButton
+						color="secondary"
+						variant="subtle"
+						icon="mdi:progress-question"
+						@click="startTour('prompt-profile')"
+					/>
+				</div>
 			</div>
 			<USeparator class="my-4 mx-3 w-4/5" />
 		</div>
 		<div class="flex flex-col items-center justify-center min-w-100 w-3/5 my-8">
 			<div
-				v-for="response in responses"
+				v-for="(response, i) in responses"
+				:id="`response-${i}`"
 				:key="response.id"
 				class="w-full mx-3 my-2"
 			>
@@ -65,6 +75,14 @@
 				Loading more responses...
 			</p>
 		</div>
+
+		<ClientOnly>
+			<SiteTour
+				:steps="promptTour"
+				name="Prompt Tour"
+				tour-id="prompt-profile"
+			/>
+		</ClientOnly>
 	</div>
 </template>
 
@@ -181,4 +199,31 @@ onMounted(async () => {
 onBeforeUnmount(() => {
 	if (observer) observer.disconnect();
 });
+
+// prompt tour
+
+const { startTour } = useSiteTour();
+
+const promptTour: SiteTourStep[] = [
+	{
+		title: 'Welcome to Prompts!',
+		description:
+			'Prompts are a fun way to ask intellectual questions and share creative ideas with the community. This card displays the prompt details, including the prompt question, description, and any associated media.',
+		footer: 'You can read the prompt and scroll down to see how others have responded to it!'
+	},
+	{
+		id: 'response-input',
+		title: 'Write a Response',
+		description:
+			'If you have a thought or idea related to the prompt, you can share it by writing a response in the input box. Just type your response and click the "Post" button to share it with the community!',
+		footer: 'Try posting your own response to the prompt!'
+	},
+	{
+		id: 'post-button',
+		title: 'Posting a Response',
+		description:
+			'After you write your response, click the "Post" button to share it with the community. Your response will appear below, and others can read it and even reply to it!',
+		footer: 'Go ahead and post your response to join the conversation!'
+	}
+];
 </script>
