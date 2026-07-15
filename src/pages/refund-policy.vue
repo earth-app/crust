405cac49e50b9300a64a0d9ada139c653fc336c5
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-07-14 21:49:14 -0500

    feat: create refund policy page

---
+++ src/pages/refund-policy.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="flex flex-col items-center w-full h-full py-4 px-16">
+		<h1 class="text-3xl font-bold">Refund Policy</h1>
+		<div class="relative size-48 my-24">
+			<EarthCircle />
+		</div>
+		<LazyLegalRefundPolicyBody hydrate-on-visible />
+	</div>
+</template>
+
+<script setup lang="ts">
+const { setTitleSuffix } = useTitleSuffix();
+setTitleSuffix('Refund Policy');
+</script>
